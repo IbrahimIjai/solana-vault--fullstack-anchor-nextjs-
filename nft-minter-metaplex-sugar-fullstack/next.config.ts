3a9267b527a2f413c274991fa480204a2524d6ad
ibrahimijai <sunday.m1701072@st.futminna.edu.ng>
2025-05-24 22:30:32 +0100

    updated nuxt config for test immage

--- nft-minter-metaplex-sugar-fullstack/next.config.ts
+++ nft-minter-metaplex-sugar-fullstack/next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'via.placeholder.com',
+      'picsum.photos',
+      'source.unsplash.com',
+      'cloudflare-ipfs.com',
+      'arweave.net',
+      'ipfs.io',
+      'cdn.pixabay.com',
+      'solana-nft.imgix.net'
+    ],
+  },
+  reactStrictMode: true,
+  swcMinify: true
 }
 
 export default nextConfig
