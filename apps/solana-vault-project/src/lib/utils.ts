d1c36591631e7640eaf39988026c37145cf123c3
ibrahimsunday <ibrahimsundayijai@gmail.com>
2025-08-29 18:59:31 +0100

    build errors on vault -1

--- apps/solana-vault-project/src/lib/utils.ts
+++ apps/solana-vault-project/src/lib/utils.ts
@@ -1,9 +1,4 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
 
 export function ellipsify(str = '', len = 4, delimiter = '..') {
   const strLen = str.length
